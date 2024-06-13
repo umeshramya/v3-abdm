008a4f542b1b31d6535339c98a49cfe130c9158a
umeshramya <umeshbilagi@gmail.com>
2024-06-13 19:50:16 +0530

    done

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as mqEncrypt } from './encrypts/m1-encrypt';
 export {default as getAaDharOtp } from "./m1/getAadharOtp"
 export {default as accesToken} from "./accessToken"
 
-export {default as enrolByAadharVerification} from "./accessToken"
+export {default as enrolByAadharVerification} from "./m1/enrollByAadharOtp"
